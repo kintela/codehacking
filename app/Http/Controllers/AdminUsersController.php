aa562f438cfc159786e3cee4b1cc341595a18fcd
kintela <rquintela@fulcrum.es>
2017-01-12 11:02:28 +0100

    delete users completed deleting images too

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -8,8 +8,8 @@ use App\Photo;
 use App\Role;
 use App\User;
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use Illuminate\Support\Facades\Session;
 
 class AdminUsersController extends Controller
 {
@@ -140,7 +140,13 @@ class AdminUsersController extends Controller
      */
     public function destroy($id)
     {
-        $user=User::findOrFail($id)->delete();
+        $user=User::findOrFail($id);
+
+        unlink(public_path() . $user->photo->file);
+
+        $user->delete();
+
+        Session::flash('deleted_user','The user has been deleted');
 
         return redirect('/admin/users');
     }
