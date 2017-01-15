48e3972c8d3d7dceb27addd0a1d9d3cc6802c910
kintela <ROBERTO.QUINTELA@GMAIL.COM>
2017-01-15 17:33:02 +0100

    Mostar los comentarios
    he cambiado el tipo del campo author a string para que salga el nombre
    en vez del user.id

--- app/Comment.php
+++ app/Comment.php
@@ -11,7 +11,8 @@ class Comment extends Model
         'author',
         'email',
         'body',
-        'is_active'
+        'is_active',
+        'file'
     ];
 
     public function replies(){
---
+++ database/migrations/2017_01_15_160732_change_type_column_author_in_comments.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeTypeColumnAuthorInComments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->string('author')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->integer('author')->change();
+        });
+    }
+}
