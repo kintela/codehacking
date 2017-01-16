0b50f90f44d34635930c746ce4037d61ea4968a1
kintela <ROBERTO.QUINTELA@GMAIL.COM>
2017-01-16 22:27:45 +0100

    create reply
    He tenido que añadir el campo photo y cambiar de tipo author que estaba
    como integer. Así que hay migraciones por medio que ejecutar

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -140,6 +140,8 @@ class AdminPostsController extends Controller
 
         $post=Post::findOrFail($id);
 
-        return view('post',compact('post'));
+        $comments=$post->comments()->whereIsActive(1)->get();
+
+        return view('post',compact('post','comments'));
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -34,43 +34,49 @@
 
     <!-- Blog Comments -->
 
-    <!-- Comments Form -->
-    <div class="well">
-        <h4>Leave a Comment:</h4>
+    @if(Auth::check())
+        <!-- Comments Form -->
+        <div class="well">
+            <h4>Leave a Comment:</h4>
 
-        {!! Form::open(['method'=>'POST', 'action'=>'PostCommentsController@store']) !!}
+            {!! Form::open(['method'=>'POST', 'action'=>'PostCommentsController@store']) !!}
 
-            <input type="hidden" name="post_id" value="{{$post->id}}">
+                <input type="hidden" name="post_id" value="{{$post->id}}">
 
-            <div class="form-group">
-                {!! Form::textarea('body',null,['class'=>'form-control','rows'=>3]) !!}
-            </div>
+                <div class="form-group">
+                    {!! Form::textarea('body',null,['class'=>'form-control','rows'=>3]) !!}
+                </div>
 
-            <div class="form-group">
-                {!! Form::submit('Submit',['class'=>'btn btn-primary']) !!}
-            </div>
+                <div class="form-group">
+                    {!! Form::submit('Submit',['class'=>'btn btn-primary']) !!}
+                </div>
 
-        {!! Form::close() !!}
+            {!! Form::close() !!}
 
-    </div>
+        </div>
 
+    @endif
     <hr>
 
     <!-- Posted Comments -->
 
-    <!-- Comment -->
-    <div class="media">
-        <a class="pull-left" href="#">
-            <img class="media-object" src="http://placehold.it/64x64" alt="">
-        </a>
-        <div class="media-body">
-            <h4 class="media-heading">Start Bootstrap
-                <small>August 25, 2014 at 9:30 PM</small>
-            </h4>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-        </div>
-    </div>
+    @if(count($comments)>0)
+        @foreach($comments as $comment)
+            <!-- Comment -->
+            <div class="media">
+                <a class="pull-left" href="#">
+                    <img height="64" class="media-object" src="{{$comment->file ? $comment->file:'http://placehold.it/64x64'}}" alt="">
+                </a>
+                <div class="media-body">
+                    <h4 class="media-heading">{{$comment->author}}
+                        <small>{{$comment->created_at->diffForHumans()}}</small>
+                    </h4>
+                    {{$comment->body}}
+                </div>
+            </div>
 
+        @endforeach
+    @endif
     <!-- Comment -->
     <div class="media">
         <a class="pull-left" href="#">
@@ -92,6 +98,22 @@
                     </h4>
                     Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
                 </div>
+
+                {!! Form::open(['method'=>'POST', 'action'=>'CommentRepliesController@createReply']) !!}
+
+                    <input type="hidden" name="comment_id" value="{{$comment->id}}">
+
+                    <div class="form-group">
+                        {!! Form::label('body','Reply:') !!}
+                        {!! Form::textarea('body',null,['class'=>'form-control','rows'=>2]) !!}
+                    </div>
+
+
+                    <div class="form-group">
+                        {!! Form::submit('submit',['class'=>'btn btn-primary']) !!}
+                    </div>
+
+                {!! Form::close() !!}
             </div>
             <!-- End Nested Comment -->
         </div>
