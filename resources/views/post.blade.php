e0e3a3a4251d9bc207c631ffc6ca184bb6db7ee6
kintela <roberto.quintela@gmail.com>
2017-01-17 15:45:51 +0100

    Maketar comments and replies

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -72,53 +72,45 @@
                         <small>{{$comment->created_at->diffForHumans()}}</small>
                     </h4>
                     {{$comment->body}}
+
+                    @if(count($comment->replies)>0)
+                        @foreach($comment->replies as $reply)
+                            <!-- Nested Comment -->
+                            <div id="nested-comment" class="media">
+                                <a class="pull-left" href="#">
+                                    <img class="media-object" height="64" src="{{$reply->photo ? $reply->photo: 'http://placehold.it/64x64'}}" alt="">
+                                </a>
+                                <div class="media-body">
+                                    <h4 class="media-heading">{{$reply->author}}
+                                        <small>{{$reply->created_at->diffForHumans()}}</small>
+                                    </h4>
+                                    {{$reply->body}}
+                                </div>
+
+                                {!! Form::open(['method'=>'POST', 'action'=>'CommentRepliesController@createReply']) !!}
+
+                                <input type="hidden" name="comment_id" value="{{$comment->id}}">
+
+                                <div class="form-group">
+                                    {!! Form::label('body','Reply:') !!}
+                                    {!! Form::textarea('body',null,['class'=>'form-control','rows'=>2]) !!}
+                                </div>
+
+
+                                <div class="form-group">
+                                    {!! Form::submit('submit',['class'=>'btn btn-primary']) !!}
+                                </div>
+
+                                {!! Form::close() !!}
+                            </div>
+                            <!-- End Nested Comment -->
+                        @endforeach
+                    @endif
                 </div>
             </div>
 
         @endforeach
     @endif
-    <!-- Comment -->
-    <div class="media">
-        <a class="pull-left" href="#">
-            <img class="media-object" src="http://placehold.it/64x64" alt="">
-        </a>
-        <div class="media-body">
-            <h4 class="media-heading">Start Bootstrap
-                <small>August 25, 2014 at 9:30 PM</small>
-            </h4>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-            <!-- Nested Comment -->
-            <div class="media">
-                <a class="pull-left" href="#">
-                    <img class="media-object" src="http://placehold.it/64x64" alt="">
-                </a>
-                <div class="media-body">
-                    <h4 class="media-heading">Nested Start Bootstrap
-                        <small>August 25, 2014 at 9:30 PM</small>
-                    </h4>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                </div>
-
-                {!! Form::open(['method'=>'POST', 'action'=>'CommentRepliesController@createReply']) !!}
-
-                    <input type="hidden" name="comment_id" value="{{$comment->id}}">
-
-                    <div class="form-group">
-                        {!! Form::label('body','Reply:') !!}
-                        {!! Form::textarea('body',null,['class'=>'form-control','rows'=>2]) !!}
-                    </div>
-
-
-                    <div class="form-group">
-                        {!! Form::submit('submit',['class'=>'btn btn-primary']) !!}
-                    </div>
-
-                {!! Form::close() !!}
-            </div>
-            <!-- End Nested Comment -->
-        </div>
-    </div>
-
 
 
 @endsection
